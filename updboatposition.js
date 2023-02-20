6567edc6a6329d407f2073c2371fc0154afe95bc
KatrinGst <106807334+KatrinGst@users.noreply.github.com>
2023-02-21 02:59:32 +0300

    v

--- updboatposition.js
+++ updboatposition.js
@@ -1,10 +1,10 @@
 const boatContainer = document.querySelector('.boat-container');
 
 function updateBoatPosition() {
-  const currentDate = new Date('January 1, 2023');
+  const currentDate = new Date();
   const travelDate = new Date('April 29, 2023');
   const timeRemaining = travelDate.getTime() - currentDate.getTime();
-  const totalTime = travelDate.getTime() - new Date('January 1, 2021').getTime();
+  const totalTime = travelDate.getTime() - new Date('January 1, 2023').getTime();
   const percentageComplete = 1 - (timeRemaining / totalTime);
   const minBottom = -150; // minimum bottom position for boat
   const maxBottom = 0; // maximum bottom position for boat
