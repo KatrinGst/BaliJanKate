49ee24162757419028c9d2857af72d358c3abb97
KatrinGst <106807334+KatrinGst@users.noreply.github.com>
2023-02-21 04:25:23 +0300

    e

--- updboatposition.js
+++ updboatposition.js
@@ -1,29 +1,14 @@
-// Set the start and end dates
 const startDate = new Date('2023-01-01');
 const endDate = new Date('2023-04-29');
+const currentDate = new Date();
+const totalTime = endDate - startDate;
+const currentTime = currentDate - startDate;
+const progress = (currentTime / totalTime) * 100;
+const runnerPosition = progress + '%';
+const runner = document.getElementById('runner');
+const progressBar = document.getElementById('progress-bar');
 
-// Get the boat, start, end, and path elements
-const boat = document.getElementById('boat');
-const start = document.getElementById('start');
-const end = document.getElementById('end');
-const path = document.getElementById('path');
+runner.style.left = runnerPosition;
+progressBar.style.width = progress + '%';
 
-// Set the start and end positions
-const startPercent = 0;
-const endPercent = 100;
-
-// Calculate the current position
-const now = new Date();
-const timeDiff = endDate.getTime() - startDate.getTime();
-const timeElapsed = now.getTime() - startDate.getTime();
-const percentElapsed = (timeElapsed / timeDiff) * 100;
-const boatPosition = startPercent + ((endPercent - startPercent) * (percentElapsed / 100));
-
-// Update the boat and path positions
-boat.style.left = `${boatPosition}%`;
-path.style.width = `${boatPosition - 10}%`;
-
-// Set the start and end icons
-start.style.left = '0';
-end.style.right = '0';
 
